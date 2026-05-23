f82fb0f2ae561f1f86c39b4b76b04b7edbe39e03
Diego-glitch-cloud <dcalderon918@gmail.com>
2026-05-22 21:17:46 -0600

    feat: componente de comida

---
+++ snake-game/src/components/Food.jsx
@@ -0,0 +1,13 @@
+function Food({ position, cellSize }) {
+  const style = {
+    position: 'absolute',
+    left: position.x * cellSize,
+    top: position.y * cellSize,
+    width: cellSize,
+    height: cellSize,
+  };
+
+  return <div className="food" style={style} />;
+}
+
+export default Food;
